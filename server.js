f163fa43ffb1064ad0ca184dddb58a6cdae4289d
tinaluis08-maker <tinaluis08@gmail.com>
2026-03-25 00:52:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,11 @@ app.get('/products', async (req, res) => {
     const result = await pool.query('SELECT * FROM products');
     res.json(result.rows);
   } catch (err) {
-    console.error(err);
+    console.error("ERRO NO BANCO:", err);
     res.status(500).json({ erro: err.message });
   }
 });
 
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3001, () => {
+  console.log('Servidor rodando');
+});
