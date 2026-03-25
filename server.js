cafd0c8f281c199aebd4d16724ced4995463615b
tinaluis08-maker <tinaluis08@gmail.com>
2026-03-24 23:59:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get('/', (req, res) => {
+  res.send('API funcionando');
+});
+
+app.get('/products', async (req, res) => {
+  const result = await pool.query('SELECT * FROM products');
+  res.json(result.rows);
+});
+
+app.listen(process.env.PORT || 3001);
