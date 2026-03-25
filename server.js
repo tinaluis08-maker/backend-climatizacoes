ead7c8b51aa8e8934b62c65065fcc75770fa15a1
tinaluis08-maker <tinaluis08@gmail.com>
2026-03-25 02:06:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
-const express = require('express');
-const { Pool } = require('pg');
-const cors = require('cors');
+const connectionString = process.env.DATABASE_URL;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+console.log("VALOR REAL:", connectionString);
+
+if (!connectionString) {
+  console.error("❌ DATABASE_URL NÃO EXISTE");
+}
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_FIX,
+  connectionString,
   ssl: { rejectUnauthorized: false }
 });
-
-app.get('/', (req, res) => {
-  res.send('API funcionando');
-});
-
-app.get('/products', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM products');
-    res.json(result.rows);
-  } catch (err) {
-    console.error("ERRO NO BANCO:", err);
-    res.status(500).json({ erro: err.message });
-  }
-});
-
-console.log("URL DO BANCO:", process.env.DATABASE_URL);
-
-app.listen(process.env.PORT || 3001, () => {
-  console.log('Servidor rodando');
-});
-
-console.log("URL DO BANCO:", process.env.DATABASE_URL);
